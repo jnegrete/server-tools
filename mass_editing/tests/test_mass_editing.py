42364ec56404e7c7f7155eb655d35b1c998d8a7d
Juan Negrete <negrete.juan85@gmail.com>
2017-11-16 16:59:47 -0600

    fix lint

--- mass_editing/tests/test_mass_editing.py
+++ mass_editing/tests/test_mass_editing.py
@@ -141,7 +141,7 @@ class TestMassEditing(common.TransactionCase):
 
     def test_sidebar_action(self):
         """Test if Sidebar Action is added / removed to / from give object."""
-        action = self.mass.ref_ir_act_window_id and \
+        action = self.mass.ref_ir_act_window_id and\
                  self.mass.ref_ir_act_window_id.binding_model_id
         self.assertTrue(action, 'Sidebar action must be exists.')
         # Remove the sidebar actions
